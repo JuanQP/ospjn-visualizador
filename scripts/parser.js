7ff0f1202b83e15b0c270fc99f95010e3843d117
JuanQP <quinterosp.juan@gmail.com>
2019-10-04 19:00:12 -0300

    Se actualiza script para obtener datos de OSPJN

--- scripts/parser.js
+++ scripts/parser.js
@@ -1,13 +1,13 @@
-/*// Una sola vez...
+// Una sola vez...
 var script = document.createElement('script');script.src = "https://code.jquery.com/jquery-3.4.1.min.js";document.getElementsByTagName('head')[0].appendChild(script);
 
-// Ir cambiando el temp0 por el tbody donde están las tr que nos interesan
-$(temp0).find("tr").toArray().map(fila => $(fila).find("td")).map(celdas => {
+// Agarra los tr del tbody donde se muestran los resultados
+$("#resultados").contents().find("tbody").eq(2).find("tr").toArray().map(fila => $(fila).find("td")).map(celdas => {
 	return {
 		"nombre": celdas.eq(1).text(),
 		"domicilio": celdas.eq(2).text().replace(/( P\:.*)$/, ""),
 		"domicilio_completo": celdas.eq(2).text(),
-		"telefonos": celdas.eq(3).text(),	
+		"telefonos": celdas.eq(3).text(),
+		"posicion": [],
 	};
 });
-*/
